164d5e82a210714eacbd5e49f38e30d4834da538
Alessandro Schena <alessandro.schena.97@protonmail.com>
2026-01-01 17:58:30 +0100

    set format-on-save as default

--- config/formatting.nix
+++ config/formatting.nix
@@ -1,6 +1,9 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    settings.default_format_opts.lsp_format = "last";
+    settings = {
+      format_on_save = { lsp_format = "fallback"; };
+      default_format_opts.lsp_format = "fallback";
+    };
   };
 }
