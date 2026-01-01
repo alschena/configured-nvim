f0a0bca4c3eb1c0813cf052ae2b2ba76d97e9c2c
Alessandro Schena <alessandro.schena.97@protonmail.com>
2026-01-01 19:17:59 +0100

    fix grammar lsp

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,22 +1,29 @@
-{lib, ...}:
-{
-    plugins.lspconfig.enable = true;
-    plugins.rustaceanvim.enable = true;
-    lsp = {
-      inlayHints = { enable = true; };
-      servers = {
-        "*" = {
-          config.capabilities = lib.nixvim.mkRaw
-            ''require("mini.completion").get_lsp_capabilities()'';
-        };
-        nixd.enable = true;
-        clangd.enable = true;
-# Grammar
-        ltex-ls.enable = true;
-# Python
-        ty.enable = true;
-        ruff.enable = true;
-        py-lsp.enable = true;
+{ lib, ... }: {
+  # Common configurations
+  plugins.lspconfig.enable = true;
+  # Rust
+  plugins.rustaceanvim.enable = true;
+  # Grammar
+  plugins.ltex-extra = {
+    enable = true;
+    settings.load-langs = [ "en-US" "it" ];
+  };
+
+  lsp = {
+    inlayHints = { enable = true; };
+    servers = {
+      "*" = {
+        config.capabilities = lib.nixvim.mkRaw
+          ''require("mini.completion").get_lsp_capabilities()'';
       };
+      nixd.enable = true;
+      clangd.enable = true;
+      # Grammar
+      ltex.enable = true;
+      # Python
+      ty.enable = true;
+      ruff.enable = true;
+      py-lsp.enable = true;
     };
+  };
 }
