b4d584f144cdefd82a24846f0b4ff0a72d54ba0c
Alessandro Schena <alessandro.schena.97@protonmail.com>
2026-01-09 21:36:01 +0100

    add treesitter functionality: grammars, selection

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,3 +1,33 @@
-{
-    plugins.treesitter.enable = true;
+{ pkgs, ... }: {
+  plugins.treesitter = {
+    enable = true;
+    highlight.enable = true;
+    indent.enable = true;
+    folding.enable = true;
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      bash
+      json
+      lua
+      nix
+      toml
+      xml
+      markdown
+      make
+      c
+      cpp
+      rust
+      python
+    ];
+    settings = {
+      incremental_selection = {
+        enable = true;
+        keymaps = {
+          init_selection = "<M-o>";
+          node_incremental = "<M-o>";
+          scope_incremental = "<M-p>";
+          node_decremental = "<M-i>";
+        };
+      };
+    };
+  };
 }
